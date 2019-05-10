412cb5d051f40f28b2da1798c63170d129614615
Cristiano <cristianovelkan@gmail.com>
2019-05-10 16:28:51 -0300

    níveis por nacionalidade

--- app/Http/Controllers/User/UserLevelController.php
+++ app/Http/Controllers/User/UserLevelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Enum\EnumUserLevelType;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserLevelRequest;
 use App\Models\User\UserLevel;
@@ -12,10 +13,14 @@ class UserLevelController extends Controller
     public function index()
     {
         try {
+
+            $type = auth()->user()->country_id == 31 ? EnumUserLevelType::NACIONAL : EnumUserLevelType::INTERNACIONAL;
+
             $levels = UserLevel::with('product')
                 ->whereHas('product', function ($product){
                     return $product->where('is_active', true);
                 })
+                ->where('type', $type)
                 ->orderBy('product_id')->get();
             return response($levels, Response::HTTP_OK);
         } catch (\Exception $e) {
