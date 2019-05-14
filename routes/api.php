a61b977e17d69ee62c98284202d054314e9c41ad
Cristiano <cristianovelkan@gmail.com>
2019-05-14 10:14:12 -0300

    remove cors from api

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,6 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 Route::post('/login', 'AuthController@login')->middleware('throttle');
 Route::get('/logout', 'AuthController@logout')->middleware('auth:api');
