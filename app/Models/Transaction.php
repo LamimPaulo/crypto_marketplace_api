c816a5e1dd91cd50dd91a07736b10f64b32eca69
cristiano <cristianovelkan@gmail.com>
2019-08-08 15:18:24 -0300

    order transactions by amount

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -243,6 +243,7 @@ class Transaction extends Model
             'coin_id' => $coin_id
         ])->with(['wallet', 'user'])
             ->take(2)
+            ->orderBy('amount')
             ->get();
     }
 
