59a4c63cdfb0f5852f1f11662a200f9c37282dc0
cristiano <cristianovelkan@gmail.com>
2019-08-08 15:57:15 -0300

    test 10 transactions

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -242,7 +242,7 @@ class Transaction extends Model
             'category' => EnumTransactionCategory::TRANSACTION,
             'coin_id' => $coin_id
         ])->with(['wallet', 'user'])
-            ->take(5)
+            ->take(10)
             ->orderBy('amount')
             ->get();
     }
