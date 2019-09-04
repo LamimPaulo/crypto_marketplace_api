83812d608cee3c434d52ab10af578393a8f86ca5
cristiano <cristianovelkan@gmail.com>
2019-09-04 09:23:36 -0300

    block cors

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,31 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Laravel CORS
+|--------------------------------------------------------------------------
+|
+| allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+| to accept any value.
+|
+*/
 
+
+if (env('APP_ENV') == 'local') {
+    return [
+
+        'supportsCredentials' => false,
+        'allowedOrigins' => ['*'],
+        'allowedHeaders' => ['*'],
+        'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
+        'exposedHeaders' => [],
+        'maxAge' => 0,
+
+    ];
+}
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Laravel CORS
-    |--------------------------------------------------------------------------
-    |
-    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
-    | to accept any value.
-    |
-    */
-
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['exchange.liquidex.com.br','admin.liquidex.com.br'],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
     'exposedHeaders' => [],
